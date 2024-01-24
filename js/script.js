d57827eb05fc6e429c10a317c312ba33d68c102a
MdOmor Faruque <mdomor.faruque@rci.rogers.com>
2024-01-24 16:07:26 -0500

    hero animation

--- js/script.js
+++ js/script.js
@@ -1,15 +1,48 @@
-function menuToggle() {
-    let x = document.getElementById("menu-content");
-    let openIcon = document.getElementById("menu-open");
-    let closeIcon = document.getElementById("menu-close");
+function toggleMenu() {
+  let x = document.getElementById("menu-content");
+  let openIcon = document.getElementById("menu-open");
+  let closeIcon = document.getElementById("menu-close");
 
-    if (x.style.display === "block") {
-      x.style.display = "none";
-      openIcon.style.display = "block";
-      closeIcon.style.display = "none";
-    } else {
-      x.style.display = "block";
-      openIcon.style.display = "none";
-      closeIcon.style.display = "block";
-    }
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    openIcon.style.display = "block";
+    closeIcon.style.display = "none";
+  } else {
+    x.style.display = "block";
+    openIcon.style.display = "none";
+    closeIcon.style.display = "block";
   }
+}
+
+
+
+function resetMobileMenu() {
+  document.getElementById("menu-content").style.display = "none";;
+  document.getElementById("menu-open").style.display = "block";
+  document.getElementById("menu-close").style.display = "none";
+}
+
+
+
+function checkWindowWidth() {
+  // Get the current window width
+  const windowWidth = window.innerWidth;
+
+  // Check if the window width is greater than 690px
+  if (windowWidth > 690) {
+    resetMobileMenu();
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  // Initial check on page load
+  checkWindowWidth();
+
+  // Add resize event listener
+  window.addEventListener('resize', function () {
+    checkWindowWidth();
+  });
+
+});
